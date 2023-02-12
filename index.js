b2401317cb7a4101bae5d2e4dfb8af6af3a4bf8d
Yatharth Jain <yatharthjain41@gmail.com>
2023-02-12 23:03:56 +0530

    Added as requested

--- index.js
+++ index.js
@@ -1,88 +1,156 @@
-var allButtons=document.querySelectorAll("button")
-var allLinks=document.querySelectorAll("a")
-var window=document
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @noframes
+// ==/UserScript==
 
-var buttonC = document.getElementById("button-c")
-var scrollC = document.getElementById("scroll-c")
-var clickC = document.getElementById("click-c")
-var linkC = document.getElementById("link-c")
-var linkC = document.getElementById("link-c")
-var timeC = document.getElementById("time-c")
-var timeC = document.getElementById("time-c")
-var currTime = document.getElementById("currTime")
+(function() {
+    'use strict';
 
-var clicks=[]
-var scrolls=[]
-var buttonClicks={}
-var linkClicks=[]
-
-
-const tabTime = Date.now();
-currTime.innerHTML=new Date(tabTime).toUTCString();
-
-var ctime=0;
-const timer=()=>{
-    timeC.innerHTML=ctime/1000+'s';
-    setTimeout(()=>{
-        ctime=ctime+1000;
-        timer();
-    },1000)
-}
-timer()
+    // Your code here...
+    // alert("Starting");
+    var allButtons=document.querySelectorAll("button")
+    var allLinks=document.querySelectorAll("a")
+    var window=document
 
 
-const addressClick = (ev) => {
-    // console.log("Click",ev.offsetX,ev.offsetY);
-    clicks.push({
-        x:ev.offsetX,
-        y:ev.offsetY,
-    })
-    clickC.innerHTML=JSON.stringify(clicks)
-};
-const onscroll=(ev)=>{
-    // console.log("Scroll",ev.timeStamp);
-    scrolls.push({
-        timeStamp:ev.timeStamp,
-    })
-    scrollC.innerHTML=JSON.stringify(scrolls)
+    var clicks=[]
+    var scrolls=[]
+    var buttonClicks={}
+    var linkClicks=[]
 
-};
-const buttonClick=(ev)=>{
-    // console.log(ev.srcElement.type)
-    if(!buttonClicks[ev.srcElement.type]){
-        buttonClicks[ev.srcElement.type]=[]
+    const tabTime = Date.now();
+
+    var ctime=0;
+    const timer=()=>{
+        setTimeout(()=>{
+            ctime=ctime+1000;
+            timer();
+        },1000)
     }
-    buttonClicks[ev.srcElement.type].push({
-        timeStamp:ev.timeStamp
+    timer()
+
+    var monitorbtn=document.createElement('div')
+
+    monitorbtn.style.width='50px';
+    monitorbtn.style.height='50px';
+    monitorbtn.style.borderRadius='50%';
+    monitorbtn.style.position='fixed';
+    monitorbtn.style.backgroundColor=localStorage.getItem('running')=="true"?'red':'greenyellow';
+    monitorbtn.style.zIndex=1000000;
+    monitorbtn.style.bottom='20px';
+    monitorbtn.style.right='20px';
+
+    document.body.appendChild(monitorbtn)
+
+
+    monitorbtn.addEventListener('click',(ev)=>{
+        if(localStorage.getItem('running')=='true'){
+            console.log(JSON.parse(localStorage.getItem('data')))
+            localStorage.setItem('running','false')
+
+            // ######### FOR SENDING DATA TO API #############
+
+            // fetch('HereGoesTheApiLink', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Accept': 'application/json',
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify({data:localStorage.getItem('data')})
+            // })
+            // .then(response => response.json())
+            // .then(response => console.log(JSON.stringify(response)))
+
+        }
+        else{
+            localStorage.setItem('running','true')
+            localStorage.setItem('data',JSON.stringify([]))
+        }
+        monitorbtn.style.backgroundColor=localStorage.getItem('running')=="true"?'red':'greenyellow';
+
+        ev.stopPropagation();
+        
     })
-    buttonC.innerHTML=JSON.stringify(buttonClicks)
 
-}
+    const addressClick = (ev) => {
+        // console.log("Click",ev.offsetX,ev.offsetY,ev);
+        var data=JSON.parse(localStorage.getItem('data'))
+        data.push({
+            addr:ev.view.location.href,
+            action:"click",
+            time:Date.now(),
+            x:ev.offsetX,
+            y:ev.offsetY,
+        })
+        localStorage.setItem('data',JSON.stringify(data))
+    };
+    const onscroll=(ev)=>{
+        // console.log("Scroll",ev);        
+        var data=JSON.parse(localStorage.getItem('data'))
+        data.push({
+            addr:ev.target.location.href,
+            action:"scroll",
+            time:Date.now(),
+            timeStamp:ev.timeStamp,            
+        })
+        localStorage.setItem('data',JSON.stringify(data))
+    };
+    const buttonClick=(ev)=>{
+        // console.log(ev.srcElement.type,ev)
+        var data=JSON.parse(localStorage.getItem('data'))
+        data.push({
+            addr:ev.view.location.href,
+            action:"buttonClick",
+            type:ev.srcElement.type,
+            time:Date.now(),
+            timeStamp:ev.timeStamp
+        })
+        localStorage.setItem('data',JSON.stringify(data))
+        ev.stopPropagation();
 
-const linkClick=(ev)=>{
-    // console.log(ev)
-    linkClicks.push({
-        href:ev.srcElement.href
+    }
+
+    const linkClick=(ev)=>{
+        // console.log(ev)
+        var data=JSON.parse(localStorage.getItem('data'))
+        
+        data.push({
+            addr:ev.view.location.href,
+            action:"linkClick",
+            time:Date.now(),
+            href:ev.srcElement.href
+        })
+        localStorage.setItem('data',JSON.stringify(data))
+        ev.stopPropagation();
+
+    }
+
+
+    document.addEventListener("click", (ev) => {
+        addressClick(ev);
+    });
+    document.addEventListener('scroll',(ev)=>{
+        onscroll(ev);
+        // console.log(ev);
     })
-    linkC.innerHTML=JSON.stringify(linkClicks)
-
-}
-
-
-document.addEventListener("click", (ev) => {
-  addressClick(ev);
-});
-document.addEventListener('scroll',(ev)=>{
-    onscroll(ev);
-    // console.log(ev);
-})
-allButtons.forEach(e=>{
-    e.addEventListener('click',(ev)=>{
-        buttonClick(ev);
+    allButtons.forEach(e=>{
+        e.addEventListener('click',(ev)=>{
+            buttonClick(ev);
+        })
     })
-})
-allLinks.forEach(e=>{
-    e.addEventListener('click',(ev)=>{
-        linkClick(ev);
+    allLinks.forEach(e=>{
+        e.addEventListener('click',(ev)=>{
+            linkClick(ev);
+        })
     })
-})
+
+
+})();
